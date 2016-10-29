dad52873916446ab8ad3e388e5dbf4d5b4a95eed
AJRiley <ajriley774@gmail.com>
2016-10-29 16:25:08 +0100

    update2

---
+++ tutorials/html-basics.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+
+<body>
+<head>
+  <link rel="stylesheet" href="../css/extra.css">
+  <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.scss">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Raleway:400,200' rel='stylesheet' type='text/css'>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+  <meta charset="UTF-8">
+
+</head>
+<div class="hold">
+  <div class="header">
+    <div class="container1">
+      <div id="logo">
+      <p>Code On Command</p>
+    </div>
+      <ul class="nav">
+        <li style="font-family:Specify PERSONAL Normal"><a href="#">Tutorials</a></li>
+        <li style="font-family:Specify PERSONAL Normal"><a href="#">Blog</a></li>
+        <li style="font-family:Specify PERSONAL Normal"><a href="#">About us</a></li>
+      </ul>
+         <ul class="nav" style="position:relative;float:right;">
+        <li style="font-family:Specify PERSONAL Normal"><a href="#">Login</a></li>
+        <li style="font-family:Specify PERSONAL Normal;background-color :rgba(156, 156, 156,0.2);"><a href="users/login" id="register1">Register for free</a></li>
+      </ul>
+    </div>
+  </div>
+    </div>
+
+<div class="section bg" style="background-color:lightgray;">
+    
+  <div class="container" style="background-color:#fff;top:150px;margin-bottom:150px;margin-left:20px;">
+    <h1>Tutorial - HTML Basics</h1>
+      <p id="para1">Posted by <span class="glyphicon glyphicon-user"></span> <a href="#">Adam Riley</a> on <span class="glyphicon glyphicon-time"></span> 12 January 2015 10:00 am</p>
+    <h2>Web development</h2>
+    <div class"content-article" style="margin-left:20px;margin-right:20px;">
+      
+      <h1>Hello, </h1>
+      <p>Proin ac turpis vel massa sollicitudin suscipit. Sed dictum dolor sed placerat varius. Phasellus dapibus vestibulum massa, non pulvinar sapien interdum sed. Cras condimentum tristique odio, non fringilla ante vulputate a. Praesent commodo arcu quis interdum interdum. Etiam aliquet eleifend velit. Quisque euismod finibus nulla, vel cursus ante egestas nec. Pellentesque quis vehicula eros, non tempor enim. Aliquam vel interdum elit, ac viverra diam. Nullam sagittis non nulla tincidunt consectetur. Cras tempor dignissim mauris, in varius massa malesuada vitae. Suspendisse nec neque sed magna fermentum blandit. Nullam in hendrerit ipsum, tincidunt elementum erat. Maecenas odio nisi, ullamcorper vel tellus non, sollicitudin viverra urna. Mauris in magna blandit, cursus leo quis, cursus turpis.</p>
+        
+      
+      </div>
+      <div class="container" style="background-color:#fff;">
+    <h1>Tutorial - HTML Basics</h1>
+    <h2>Web development</h2>
+    <div class"content-article">
+      
+      <h1>Hello, </h1>
+      <p>Proin ac turpis vel massa sollicitudin suscipit. Sed dictum dolor sed placerat varius. Phasellus dapibus vestibulum massa, non pulvinar sapien interdum sed. Cras condimentum tristique odio, non fringilla ante vulputate a. Praesent commodo arcu quis interdum interdum. Etiam aliquet eleifend velit. Quisque euismod finibus nulla, vel cursus ante egestas nec. Pellentesque quis vehicula eros, non tempor enim. Aliquam vel interdum elit, ac viverra diam. Nullam sagittis non nulla tincidunt consectetur. Cras tempor dignissim mauris, in varius massa malesuada vitae. Suspendisse nec neque sed magna fermentum blandit. Nullam in hendrerit ipsum, tincidunt elementum erat. Maecenas odio nisi, ullamcorper vel tellus non, sollicitudin viverra urna. Mauris in magna blandit, cursus leo quis, cursus turpis.</p>
+        
+      
+      </div>
+</div>
+<div class="section">
+  <div class="container">
+    <h1 class="reset">Terrible.</h1>
+  </div>
+</div>
+    </div>
+    <div class="section">
+  <div class="footer">
+    <div class="container white">
+      <div class="col four left">
+        <h1>What?</h1>
+        <p>So that's it. This started out as 20 minutes of making fun of modern web dev. Then it turned into a few hours of it.</p>
+        <p>I hope you've enjoyed looking at every modern website ever.</p>
+        <p>I don't actually hate this style as long as the content is meaningful. In fact, I think this type of design is pretty cool and effective.</p>
+      </div>
+      <div class="col four left">
+        <h1>How?</h1>
+        <p>CSS3 and HTML. JS for header shrinking; optional. Site works perfectly with JS disabled (another gripe of mine with modern web dev).</p>
+        <p>Only external libraries are GFonts.</p>
+        <p>Moderately responsive, should work on anything modern.</p>
+      </div>
+      <div class="col four left">
+        <h1>Why?</h1>
+        <p>Many popular HTML themes have thousands of lines of HTML, thousands of lines of CSS and several JS plugins on every page amounting to tens of thousands of lines of JavaScript.</p>
+        <p>I fail to see a valid reason for this, particularly the horrendous line counts that are usually due to unused features or badly designed code.</p>
+      </div>
+      <div class="col four left">
+        <h1>Who?</h1>
+        <p>I'm Adam</p>
+        <p>You can get in touch with me through <a href="http://atunnecliffe.com">http://atunnecliffe.com</a> or <a href="mailto:andrew@atunnecliffe.com">emailing me</a>.</p>
+      </div>
+      <div class="group"></div>
+    </div>
+  </div>
+</div>
+<body>
+
+<script src="../js/main.js"></script>
+
+
+</html>
